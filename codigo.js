a35f760270abfd38fc0049152e81caeedafb10d5
JesusMNA <jesuso_0709@hotmail.com>
2022-07-12 23:54:09 -0500

    Se arreglo el sonido

--- codigo.js
+++ codigo.js
@@ -38,8 +38,6 @@ const respuesta2 = document.getElementById("respuesta2");
 const respuesta3 = document.getElementById("respuesta3");
 const sonido = document.getElementById("sonido");
 
-sonido.play();
-
 sonido.volume = 0.25;
 
 sonido.addEventListener("ended", () => {
@@ -62,6 +60,7 @@ const jugador3 = new Jugador({ nombre: "", personaje: "", numeroJugador: 3 });
 const jugador4 = new Jugador({ nombre: "", personaje: "", numeroJugador: 4 });
 
 btnIniciar.addEventListener("click", () => {
+    sonido.play();
     mInicio.classList.remove("inicio--show");
     pJugadores.classList.add("jugadores--show");
 })
